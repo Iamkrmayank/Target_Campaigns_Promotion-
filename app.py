3cb98ea87128224ccc5b671f5f119d44d2c245d6
Kumar Mayank <103871423+Iamkrmayank@users.noreply.github.com>
2024-08-16 16:14:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -160,10 +160,7 @@ def transactional_visualizations(df):
 
         last_purchase_date = df['Transaction Date'].max()
         df['Churn'] = (last_purchase_date - df['Transaction Date']).dt.days > 365
-    else:
-        st.warning("'Transaction Date' column is not present in the dataset.")
-
-
+    
     customer_segmentation(df)
     geolocation_segmentation(df)
     time_based_segmentation(df)
